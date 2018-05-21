f218618cc28b297aa41cfb5d8dbebecdaaed2b79
andgrand <grddron@gmail.com>
2018-05-21 08:46:46 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <string>
-//#include "algo.h"
+#include "algo.h"
 
 using namespace std;
 
 int main(int argc, char* args[]) {
     try {
-        throw string("123");
-//        algo::OpenFile(string(args[1]));
+        algo::OpenFile(string(args[1]));
         try {
-//            algo::MainAlgorithm();
+            algo::MainAlgorithm();
         }
         catch (string &exp) {
             cout << "Error: " << exp << endl;
