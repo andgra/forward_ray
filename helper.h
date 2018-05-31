1d87d84482a69ad1c21d89186f597b0adb7e8f02
andgrand <grddron@gmail.com>
2018-05-31 22:25:49 +0300

    timer unix

--- helper.h
+++ helper.h
@@ -56,6 +56,7 @@ bool containsKey(unordered_map<K, V> m, K k) {
 }
 
 
+#if defined(_WIN32)
 #include <iostream>
 #include <chrono>
 
@@ -73,5 +74,27 @@ private:
     typedef std::chrono::duration<double, std::ratio<1> > second_;
     std::chrono::time_point<clock_> beg_;
 };
+
+#else
+#include <iostream>
+#include <ctime>
+
+class Timer
+{
+public:
+    Timer() { clock_gettime(CLOCK_REALTIME, &beg_); }
+
+    double elapsed() {
+        clock_gettime(CLOCK_REALTIME, &end_);
+        return end_.tv_sec - beg_.tv_sec +
+               (end_.tv_nsec - beg_.tv_nsec) / 1000000000.;
+    }
+
+    void reset() { clock_gettime(CLOCK_REALTIME, &beg_); }
+
+private:
+    timespec beg_, end_;
+};
+#endif
 
 #endif //HELPER_H
